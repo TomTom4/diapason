9602d2da6807093db767e02af7e94ae9f9809559
TomPouv <contact@thomas-pouvreau.fr>
2025-04-23 11:49:49 +0200

    feat: show up sound from mic :)

--- src/components/AudioVisualizer.tsx
+++ src/components/AudioVisualizer.tsx
@@ -1,28 +1,24 @@
-import { useState, useEffect } from "react";
+import { useRef, Ref } from "react";
+import { AnalyzeSound } from "./AudioStream";
 
 export default function AudioVisualizer() {
-  const [audioDataArray, setAudioDataArray] = useState(undefined);
-  const audioCtx = new AudioContext();
-  const analyser = audioCtx.createAnalyser();
+  const canvasRef: Ref<HTMLCanvasElement> = useRef(null);
 
-  useEffect(() => {
-    const source = audioCtx.createMediaStreamSource(stream);
-    source.connect(analyser);
-
-    analyser.fftSize = 2048;
-    const bufferLength = analyser.frequencyBinCount;
-    const dataArray = new Uint8Array(bufferLength);
-    setAudioDataArray(dataArray);
-  }, []);
-
-  const tic = () => {
-    const datayArray = new Uint8Array();
-    analyser.getByteTimeDomainData(dataArray);
+  const start = async () => {
+    if (canvasRef !== null) {
+      if (canvasRef.current !== null) {
+        await AnalyzeSound(canvasRef.current.getContext("2d"), 550, 200);
+      }
+    }
   };
 
   return (
     <div>
       <p>this is the audio visualizer</p>
+      <canvas ref={canvasRef} width={550} height={200} />
+      <button onClick={start} type="button">
+        start
+      </button>
     </div>
   );
 }
