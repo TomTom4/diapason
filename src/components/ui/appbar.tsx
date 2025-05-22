3c6ab3b10f57da0c20c455ef9f418e3d63131c0f
TomPouv <contact@thomas-pouvreau.fr>
2025-05-22 14:22:09 +0200

    style: fine tune elements sizes

--- src/components/ui/appbar.tsx
+++ src/components/ui/appbar.tsx
@@ -4,10 +4,10 @@ import SoundWave from "@/assets/soundwave.png";
 export default function AppBar() {
   return (
     <div className="w-auto">
-      <div className="flex justify-between p-4">
+      <div className="flex justify-between p-4 items-center">
         {" "}
-        <img src={SoundWave} className="size-10" />
-        <h2 className="text-2xl font-bold font-stretch-50%">Diapason</h2>
+        <img src={SoundWave} className="size-12" />
+        <h2 className="text-3xl font-bold font-stretch-50%">Diapason</h2>
         <Bars3Icon className="size-7" />
       </div>
     </div>
