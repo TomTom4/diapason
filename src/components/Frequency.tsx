d78bd2b27418182291778db38088ad19df893e05
TomPouv <contact@thomas-pouvreau.fr>
2025-05-22 17:46:42 +0200

    feat: grep the fundamental and show its value correctly

--- src/components/Frequency.tsx
+++ src/components/Frequency.tsx
@@ -7,43 +7,14 @@ const stream = await getMicStream();
 
 const source = context.createMediaStreamSource(stream);
 source.connect(analyser);
-analyser.fftSize = 4096;
+analyser.fftSize = 4096 * 2;
 
-function autoCorrelate(buf: Float32Array, sampleRate: number) {
-  const SIZE = buf.length;
-  let rms = 0;
-
-  for (let i = 0; i < SIZE; i++) {
-    const val = buf[i];
-    rms += val * val;
-  }
-  rms = Math.sqrt(rms / SIZE);
-  if (rms < 0.01) return -1; // too quiet
-
-  let bestOffset = -1;
-  let bestCorrelation = 0;
-  let correlations = new Array(SIZE).fill(0);
-
-  for (let offset = 0; offset < SIZE / 2; offset++) {
-    let correlation = 0;
-    for (let i = 0; i < SIZE / 2; i++) {
-      correlation += Math.abs(buf[i] - buf[i + offset]);
-    }
-    correlation = 1 - correlation / (SIZE / 2);
-    correlations[offset] = correlation;
-
-    if (correlation > bestCorrelation) {
-      bestCorrelation = correlation;
-      bestOffset = offset;
-    }
-  }
-
-  if (bestCorrelation > 0.9) {
-    return sampleRate / bestOffset;
-  }
-
-  return -1;
-}
+const extractFundamental = (buffer: Uint8Array) => {
+  console.log(analyser.frequencyBinCount);
+  const fundamentalPike = Math.max(...buffer);
+  const index = buffer.indexOf(fundamentalPike);
+  return (index * 12000) / analyser.frequencyBinCount;
+};
 
 export default function Frequency() {
   const [frequency, setFrequency] = useState<number | null>(null);
@@ -58,11 +29,11 @@ export default function Frequency() {
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      const bufferLength = analyser.fftSize;
-      const data = new Float32Array(bufferLength);
-      analyser.getFloatTimeDomainData(data);
-      const pitch = autoCorrelate(data, context.sampleRate);
-      setFrequency(pitch);
+      const bufferLength = analyser.frequencyBinCount;
+      const data = new Uint8Array(bufferLength);
+      analyser.getByteFrequencyData(data);
+      const fundamental = extractFundamental(data);
+      setFrequency(fundamental);
     }, 100);
   }, []);
 
