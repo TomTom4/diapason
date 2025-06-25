5de112a29c3c396bbe5dd918f085ede31b3c76a5
TomPouv <contact@thomas-pouvreau.fr>
2025-06-25 11:58:35 +0200

    feat: build a first draft of the manifest configurations

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,20 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
-    VitePWA({ registerType: "autoUpdate", devOptions: { enabled: true } }),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: { enabled: true },
+      manifest: {
+        name: "Diapason, your favorite online tuner",
+        short_name: "Diapason",
+        description: "A simple yet practical online tuner",
+        theme_color: "#fff",
+        icons: [
+          { src: "logo-192*192.png", sizes: "192x192", type: "image/png" },
+          { src: "logo-512*512.png", sizes: "512x512", type: "image/png" },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
